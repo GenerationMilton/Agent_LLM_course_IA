93213fb412d18e396ad06d576425c6864abebd76
Milton Munoz <livemilton@gmail.com>
2026-03-02 22:17:38 -0500

    IA AGENTS: :sparkles: The Gist of IA Agents - Integrating Real-world search with tavily and LangChain Tools

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
+from tavily import TavilyClient
+# Another tools without @tool
+#from langchain_tavily import TavilySearch
+
+#initialize tavily
+tavily = TavilyClient()
 
 @tool
 def search(query: str) -> str:
@@ -16,16 +22,19 @@ def search(query: str) -> str:
         The search result
     """
     print(f"Searching for {query}")
-    return("Tokyo weather is sunny")
+    return tavily.search(query=query)
+    #return("Tokyo weather is sunny")
 
 llm = ChatOpenAI(model="gpt-5")
 tools = [search]
+# Another tools
+#tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages":HumanMessage(content="What is the weather in Tokyo")})
+    result = agent.invoke({"messages":HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details")})
     print(result)
 
 
